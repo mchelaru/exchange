35d1c45ad3e45becdd0e64555f122c812c003603
Mihai Chelaru <mihaiche@yahoo.com>
2024-10-11 08:29:03 +0200

    order, not trade

--- disseminator/src/disseminator.rs
+++ disseminator/src/disseminator.rs
@@ -5,7 +5,7 @@ use order::Order;
 pub trait Disseminator: std::fmt::Debug {
     fn send_cancel_order(&self, order: &Order) -> Result<usize, std::io::Error>;
     fn send_new_order(&self, order: &Order) -> Result<usize, std::io::Error>;
-    fn send_modify_order(&self, trade: &Order) -> Result<usize, std::io::Error>;
+    fn send_modify_order(&self, order: &Order) -> Result<usize, std::io::Error>;
     fn send_trade(&self, trade: &Trade) -> Result<usize, std::io::Error>;
 
     // instruments and snapshots
